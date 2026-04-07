3797af5b43f0d7673c9d47aef1a8e4cea1cdf660
Anshu Tewari <atewari@phdata.io>
2026-04-07 14:07:50 +0530

    Terraform build check

--- modules/azure_infra/variable.tf
+++ modules/azure_infra/variable.tf
@@ -1,6 +1,32 @@
-variable "prefix"         { type = string }
-variable "rg_name"        { type = string }
-variable "location"       { type = string }
-variable "vnet_id"        { type = string }
-variable "public_subnet"  { type = string }
-variable "private_subnet" { type = string }
+variable "prefix" {
+  type = string
+}
+
+variable "rg_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "vnet_id" {
+  type = string
+}
+
+variable "public_subnet" {
+  type = string
+}
+
+variable "private_subnet" {
+  type = string
+}
+
+# --- THESE ARE THE MISSING VARIABLES ---
+variable "public_subnet_nsg_association_id" {
+  type = string
+}
+
+variable "private_subnet_nsg_association_id" {
+  type = string
+}
