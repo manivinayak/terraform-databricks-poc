54678d9ab878855e26c44e401c3c3c1c33ffc258
Anshu Tewari <atewari@phdata.io>
2026-04-07 13:43:28 +0530

    Terraform build check

--- global-int/main.tf
+++ global-int/main.tf
@@ -1,32 +1,47 @@
+# Needed to get Tenant ID for Key Vault
 data "azurerm_client_config" "current" {}
 
-# 1. Network Module Call
+# 1. CREATE THE RESOURCE GROUP (This was missing!)
+resource "azurerm_resource_group" "this" {
+  name     = var.resource_group
+  location = var.location
+}
+
+# 2. Create Networking First
 module "network" {
   source              = "../modules/networking"
   prefix              = "kiewit-${var.environment}"
-  rg_name             = var.resource_group
-  location            = var.location
+  
+  # Using azurerm_resource_group.this.name forces Terraform to wait 
+  # until the RG is created before running this module.
+  rg_name             = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  
   vnet_cidr           = var.vnet_cidr
   public_subnet_cidr  = var.public_subnet_cidr
   private_subnet_cidr = var.private_subnet_cidr
 }
 
-# 2. Key Vault
+# 3. Create Key Vault
 resource "azurerm_key_vault" "this" {
   name                = "kv-kiewit-${var.environment}-001"
-  location            = var.location
-  resource_group_name = var.resource_group
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "standard"
+  
+  # Suggested for POC to allow easy cleanup
   purge_protection_enabled = false 
 }
 
-# 3. Base Infrastructure Module Call
+# 4. Create Storage & Workspace (Passing Network IDs)
 module "base_infra" {
   source         = "../modules/azure_infra"
   prefix         = "kiewit-${var.environment}"
-  rg_name        = var.resource_group
-  location       = var.location
+  
+  rg_name        = azurerm_resource_group.this.name
+  location       = azurerm_resource_group.this.location
+  
   vnet_id        = module.network.vnet_id
   public_subnet  = module.network.public_subnet_name
   private_subnet = module.network.private_subnet_name
